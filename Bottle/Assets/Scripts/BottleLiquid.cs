f98402bf69052f973d970b9eef38496b2c62ea27
Matt <mattstark256@gmail.com>
2020-01-04 13:49:42 +0000

    added some comments

--- Bottle/Assets/Scripts/BottleLiquid.cs
+++ Bottle/Assets/Scripts/BottleLiquid.cs
@@ -20,14 +20,17 @@ public class BottleLiquid : MonoBehaviour
 
     void Update()
     {
-        Vector3 worldN = Vector3.up;
+        // The liquid surface normal in world space (if you need it to slosh around, modify this)
+        Vector3 worldN = Quaternion.Euler(0, 0, Time.time * 180) * Vector3.up;
 
+        // The liquid surface normal in local space
         Vector3 n = transform.InverseTransformVector(worldN);
         meshRenderer.material.SetVector("_SurfaceNormal", n);
 
+        // The vector from the centre of the object to a corner of its bounding box
         Vector3 s = Vector3.Scale(meshFilter.mesh.bounds.max, transform.lossyScale);
-        
-        // This is the important bit
+
+        // heightScale is a rough approximation of the vertical distance between the centre and the highest point of the bottle
         float heightScale = Vector3.Dot(Vector3.Scale(s, n), n);
 
         float height = heightScale * (fullness * 2 - 1);
